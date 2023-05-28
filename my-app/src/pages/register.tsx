f56cbe827d13bb8fd6e7e62d64edcb5de91bbe6f
peter hagenson <peterhagenson@peters-Mac-mini.local>
2023-05-28 13:31:06 -0500

    user registration working

--- my-app/src/pages/register.tsx
+++ my-app/src/pages/register.tsx
@@ -1,20 +1,34 @@
 //create a component that registers users with an email and password
 
 import {useState} from 'react';
+import {useRegisterUserMutation} from '../services/usersApi';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function Register() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName,setLastName] = useState("");
     const [user, setUser] = useState(null);
+
+    const [registerUser] = useRegisterUserMutation();
+
+    const navigate = useNavigate();
 
     const handleSubmit = (event: any) => {
         event.preventDefault();
         const userData = {
+            firstName,
+            lastName,
             email,
-            password
+            password,
         };
-        console.log(email, password)
+        console.log(userData)
+        registerUser(userData).then(response => navigate('/login'));
+
     }
         
 
@@ -28,6 +42,14 @@ function Register() {
         </header>
         <div>
             <form onSubmit={handleSubmit}>
+                <label>
+                First Name:
+                <input type="text" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            </label>
+            <label>                                                 
+                Last Name:
+                <input type="text" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </label>
             <label>
                 Email:
                 <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
